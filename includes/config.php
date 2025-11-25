6d8a3791c7b993f6e6db18b9c49d604a0431ef8e
John Christian Vicente <johnc.vicente1@gmail.com>
2025-11-25 21:40:51 +0800

    removed session config

--- includes/config.php
+++ includes/config.php
@@ -8,9 +8,4 @@ define('DB_NAME', 'main_db');
 // Application settings
 define('APP_NAME', 'JLibrary Management');
 define('APP_VERSION', '1.0');
-
-// Session configuration
-ini_set('session.cookie_httponly', 1);
-ini_set('session.use_only_cookies', 1);
-ini_set('session.cookie_secure', 0); // Set to 1 if using HTTPS
 ?>
