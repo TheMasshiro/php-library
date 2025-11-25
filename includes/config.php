c405f56edde296ab9ea0bee249bf0d34192726f5
John Christian Vicente <johnc.vicente1@gmail.com>
2025-11-25 22:31:56 +0800

    removed comments

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,11 @@
 <?php
-// Database configuration
+
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'main_db');
 
-// Application settings
+
 define('APP_NAME', 'Jean Library Management');
 define('APP_VERSION', '1.0');
 ?>
