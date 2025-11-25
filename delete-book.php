a8cb0ea194ba5dbf36274a5ea32fd076be3d578e
John Christian Vicente <johnc.vicente1@gmail.com>
2025-11-25 23:08:58 +0800

    Final Commit

--- delete-book.php
+++ delete-book.php
@@ -6,8 +6,9 @@ include "includes/functions.php";
 require_auth();
 if (!isset($_GET['id'])) redirect('books.php');
 
+$id = intval($_GET['id']);
 $stmt = mysqli_prepare($conn, "DELETE FROM books WHERE id=?");
-mysqli_stmt_bind_param($stmt, "i", intval($_GET['id']));
+mysqli_stmt_bind_param($stmt, "i", $id);
 mysqli_stmt_execute($stmt) 
     ? redirect('books.php', 'Book deleted successfully', 'success')
     : redirect('books.php', 'Failed to delete book');
